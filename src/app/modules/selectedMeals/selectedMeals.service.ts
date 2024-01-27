2d85c3d3198fae9f482f9109470026ef242fb51e
Maruf Hossain <dev.maruf112@gmail.com>
2024-01-27 19:07:01 +0600

    save user selected meals specifically

--- src/app/modules/selectedMeals/selectedMeals.service.ts
+++ src/app/modules/selectedMeals/selectedMeals.service.ts
@@ -1,8 +1,60 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import { User } from '../user/user.model';
 import { ISelectedMeals } from './selectedMeals.interface';
 import { SelectedMeals } from './selectedMeals.model';
+import { Meal } from '../meal/meal.model';
 
 const createSelectedMeals = async (payload: ISelectedMeals) => {
-  const result = (await SelectedMeals.create(payload)).populate([
+  const userExist = await User.findById(payload.user);
+  if (!userExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'No user found');
+  }
+
+  // Filter out items that do not exist in the Meal collection
+  const validMealIds = await Meal.find({
+    _id: { $in: payload.selected_meals },
+  }).distinct('_id');
+  console.log('validMealIds', validMealIds);
+  // Check if all selected meals exist in the Meal collection
+  if (validMealIds.length !== payload.selected_meals.length) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'One or more selected meals do not exist',
+    );
+  }
+
+  // Check if any of the selected meals have already been selected by the user
+  const existingSelectedMeals = await SelectedMeals.findOne({
+    user: payload.user,
+    selected_meals: { $in: validMealIds },
+  });
+
+  if (existingSelectedMeals) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'User has already selected some of the meals',
+    );
+  }
+
+  const filteredSelectedMeals = {
+    user: payload.user,
+    selected_meals: validMealIds,
+  };
+
+  let existUserSelectedMeals = await SelectedMeals.findOne({
+    user: payload.user,
+  });
+  if (existUserSelectedMeals) {
+    existUserSelectedMeals.selected_meals = [
+      ...existUserSelectedMeals.selected_meals,
+      ...validMealIds,
+    ];
+    await existUserSelectedMeals.save();
+  } else {
+    existUserSelectedMeals = await SelectedMeals.create(filteredSelectedMeals);
+  }
+  const result = await existUserSelectedMeals.populate([
     {
       path: 'selected_meals',
     },
